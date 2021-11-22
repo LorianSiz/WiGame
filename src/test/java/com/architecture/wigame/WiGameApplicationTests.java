ef4a30456834246f70e7bfb5d1f63b6f7767b23d
lorian <lorian.sizaire@laposte.net>
2021-11-22 14:46:51 +0100

    Initial commit

---
+++ src/test/java/com/architecture/wigame/WiGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architecture.wigame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
